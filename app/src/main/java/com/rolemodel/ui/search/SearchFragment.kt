b49e206fa8509e56cf1f3ed16d9a26113f15ed1d
kazaky <tall.shahawi@gmail.com>
2019-11-27 02:04:41 +0200

    Add Search UI state logic

--- app/src/main/java/com/rolemodel/ui/search/SearchFragment.kt
+++ app/src/main/java/com/rolemodel/ui/search/SearchFragment.kt
@@ -1,6 +1,7 @@
 package com.rolemodel.ui.search
 
 import android.os.Bundle
+import android.view.View
 import com.rolemodel.R
 import com.rolemodel.data.model.Person
 import com.rolemodel.ui.base.BaseFragment
@@ -22,14 +23,28 @@ class SearchFragment : BaseFragment() {
 
 
     private fun showInitial() {
+        rvDoctors.visibility = View.GONE
+        tvEmpty.visibility = View.GONE
+        showLoading(false)
+        wInitial.visibility = View.VISIBLE
     }
 
     private fun showEmpty() {
+        rvDoctors.visibility = View.GONE
+        wInitial.visibility = View.GONE
+        tvEmpty.visibility = View.VISIBLE
+        showLoading(false)
     }
 
     private fun showLoading(shouldShow: Boolean) {
+        progressBar.visibility = if (shouldShow) View.VISIBLE else View.INVISIBLE
     }
 
     private fun showArtists(results: List<Person>) {
+        wInitial.visibility = View.GONE
+        tvEmpty.visibility = View.GONE
+        rvDoctors.visibility = View.VISIBLE
+        showLoading(false)
+        artistsAdapter.submitList(results)
     }
 }

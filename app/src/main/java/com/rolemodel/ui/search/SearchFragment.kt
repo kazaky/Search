569ef0fdf22dc19c0bc8373cf11b40aa667ee1e7
kazaky <tall.shahawi@gmail.com>
2019-11-27 02:07:20 +0200

    Handle listening to search input

--- app/src/main/java/com/rolemodel/ui/search/SearchFragment.kt
+++ app/src/main/java/com/rolemodel/ui/search/SearchFragment.kt
@@ -2,9 +2,16 @@ package com.rolemodel.ui.search
 
 import android.os.Bundle
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import androidx.lifecycle.Observer
+import com.google.android.material.snackbar.Snackbar
+import com.jakewharton.rxbinding3.widget.textChanges
 import com.rolemodel.R
 import com.rolemodel.data.model.Person
+import com.rolemodel.data.model.Status
 import com.rolemodel.ui.base.BaseFragment
+import com.rolemodel.ui.base.ScreenState
 import kotlinx.android.synthetic.main.fragment_search.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -19,31 +26,67 @@ class SearchFragment : BaseFragment() {
         // Init UI
         artistsAdapter = ArtistsListAdapter()
         rvDoctors.adapter = artistsAdapter
+
+        // Observing ViewModel state changes
+        viewModel.searchState.observe(viewLifecycleOwner, Observer { renderUI(it) })
+
+        // Start listening to search changes
+        viewModel.registerSearchListener(etSearch.textChanges())
     }
 
+    private fun renderUI(searchState: ScreenState<SearchState>?) {
+        when (searchState) {
+            is ScreenState.Loading -> showLoading(true)
+            is ScreenState.Success -> processSuccessState(searchState.data)
+            is ScreenState.Error -> processErrorState(searchState.status)
+        }
+    }
+
+    private fun processErrorState(status: Status) {
+        showLoading(false)
+
+        activity?.let { safeActivity ->
+            Snackbar.make(
+                safeActivity.findViewById(android.R.id.content),
+                status.message,
+                Snackbar.LENGTH_INDEFINITE
+            ).setAction(
+                getString(R.string.retry)
+            ) {
+                viewModel.registerSearchListener(etSearch.textChanges())
+            }.show()
+        }
+    }
+
+    private fun processSuccessState(searchState: SearchState) =
+        when (searchState) {
+            is SearchState.ShowInitial -> showInitial()
+            is SearchState.ShowPersons -> showArtists(searchState.persons)
+            is SearchState.ShowEmpty -> showEmpty()
+        }
 
     private fun showInitial() {
-        rvDoctors.visibility = View.GONE
-        tvEmpty.visibility = View.GONE
+        rvDoctors.visibility = GONE
+        tvEmpty.visibility = GONE
         showLoading(false)
-        wInitial.visibility = View.VISIBLE
+        wInitial.visibility = VISIBLE
     }
 
     private fun showEmpty() {
-        rvDoctors.visibility = View.GONE
-        wInitial.visibility = View.GONE
-        tvEmpty.visibility = View.VISIBLE
+        rvDoctors.visibility = GONE
+        wInitial.visibility = GONE
+        tvEmpty.visibility = VISIBLE
         showLoading(false)
     }
 
     private fun showLoading(shouldShow: Boolean) {
-        progressBar.visibility = if (shouldShow) View.VISIBLE else View.INVISIBLE
+        progressBar.visibility = if (shouldShow) VISIBLE else View.INVISIBLE
     }
 
     private fun showArtists(results: List<Person>) {
-        wInitial.visibility = View.GONE
-        tvEmpty.visibility = View.GONE
-        rvDoctors.visibility = View.VISIBLE
+        wInitial.visibility = GONE
+        tvEmpty.visibility = GONE
+        rvDoctors.visibility = VISIBLE
         showLoading(false)
         artistsAdapter.submitList(results)
     }
--- app/src/main/java/com/rolemodel/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/rolemodel/ui/search/SearchViewModel.kt
@@ -2,12 +2,14 @@ package com.rolemodel.ui.search
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.distinctUntilChanged
+import com.jakewharton.rxbinding3.InitialValueObservable
 import com.rolemodel.data.model.Status
 import com.rolemodel.data.repository.DataRepository
 import com.rolemodel.scheduler.SchedulerProvider
 import com.rolemodel.ui.base.BaseViewModel
 import com.rolemodel.ui.base.ScreenState
 import io.reactivex.functions.Consumer
+import java.util.concurrent.TimeUnit
 
 class SearchViewModel constructor(
     private val repository: DataRepository,
@@ -34,12 +36,42 @@ class SearchViewModel constructor(
                     safeArtists.persons?.let { safePersons ->
                         if (safePersons.isNotEmpty())
                             searchLiveData.postValue(
-                                ScreenState.Success(SearchState.ShowPersons(safePersons))
-                            )
+                                ScreenState.Success(SearchState.ShowPersons(safePersons)))
                         else
                             searchLiveData.postValue(
-                                ScreenState.Success(SearchState.ShowEmpty)
-                            )
+                                ScreenState.Success(SearchState.ShowEmpty))
+                    }
+                }
+            }, defaultErrors)
+            .let { compositeDisposable.add(it) }
+    }
+
+    fun registerSearchListener(searchObservable: InitialValueObservable<CharSequence>) {
+        searchObservable
+            .debounce(500, TimeUnit.MILLISECONDS)
+            .filter {
+                if (it.length <= 2)
+                    searchLiveData.postValue(ScreenState.Success(SearchState.ShowInitial))
+                it.length > 2
+            }
+            .switchMapSingle {
+                repository.searchArtists(it.toString())
+                    .doOnSubscribe {
+                        searchLiveData.postValue(ScreenState.Loading)
+                    }
+                    .retry(3)
+            }
+            .subscribeOn(scheduler.io())
+            .observeOn(scheduler.ui())
+            .subscribe(Consumer {
+                it?.artists?.let { safeArtists ->
+                    safeArtists.persons?.let { safePersons ->
+                        if (safePersons.isNotEmpty())
+                            searchLiveData.postValue(
+                                ScreenState.Success(SearchState.ShowPersons(safePersons)))
+                        else
+                            searchLiveData.postValue(
+                                ScreenState.Success(SearchState.ShowEmpty))
                     }
                 }
             }, defaultErrors)
